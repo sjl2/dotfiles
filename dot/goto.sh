f99e2f6f147bef68372a66ee43842b6cf165b233
Stewart Lynch <stewart.j.lynch@gmail.com>
2019-06-13 17:55:41 -0700

    feat(goto): add zsh helper

---
+++ dot/goto.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# paths in which to find projects or directories
+# top path wins
+goto-refresh-search-paths () {
+  setopt nullglob
+  goto_search_paths=(
+    $GOPATH/src/*/*
+    ~/lob
+  )
+  unsetopt nullglob
+}
+
+goto-which () {
+  goto-refresh-search-paths
+  for p in $goto_search_paths; do
+    local it="$p/$1"
+    if [ -e "$it" ] ; then
+      echo "$it"
+      return 0
+    fi
+  done
+
+  echo "goto-which: not found: $1" > /dev/stderr
+  return 1
+}
+
+goto () {
+  local dir=$(goto-which "$1")
+  if [ -n "$dir" ]; then
+    echo "cd $dir"
+    cd "$dir"
+  else
+    echo "goto: not found: $1" > /dev/stderr
+    return 1
+  fi
+}
+
+_goto () {
+  goto-refresh-search-paths
+  COMPREPLY=()
+
+  # Only complete one word
+  if [[ $COMP_CWORD -gt 1 ]]; then
+    return 0
+  fi
+
+  # Complete with matching paths inside our search paths
+  local cur
+  cur="${COMP_WORDS[COMP_CWORD]}"
+
+  local path_glob
+  local candidates
+  for p in "${goto_search_paths[@]}"; do
+    # Loop through all the search paths and see if we can glob anything
+    # beginning with the user's typing.
+    path_glob="$p/$cur*"
+    candidates=($(compgen -G "$path_glob" | xargs basename))
+    # Add any matches to the completion result
+    COMPREPLY+=( "${candidates[@]}" )
+  done
+
+  return 0
+}
+
+if [[ "$SHELL" == *"zsh" ]]; then
+  autoload bashcompinit
+  bashcompinit
+fi
+
+complete -F _goto goto
+complete -F _goto goto-which
+
+alias gt="goto"
