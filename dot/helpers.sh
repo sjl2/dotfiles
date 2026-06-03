81e9e41a2fe8b18d4e77e22a81e3794471297fe5
Stewart Lynch <stewart.j.lynch@gmail.com>
2026-06-03 10:45:49 -0700

    Refactor to simplify settings

--- dot/helpers.sh
+++ dot/helpers.sh
@@ -14,3 +14,31 @@ function mkdir_and_touch () {
 }
 
 alias mt="mkdir_and_touch"
+
+
+# Open or attach the secureframe tmux session.
+# Windows 1-10 map to ~/dev/secureframe-1..10.
+function sf () {
+  local session="sf"
+
+  if tmux has-session -t "$session" 2>/dev/null; then
+    if [ -n "$TMUX" ]; then
+      tmux switch-client -t "$session"
+    else
+      tmux attach -t "$session"
+    fi
+    return
+  fi
+
+  tmux new-session -d -s "$session" -n "sf-1" -c "$HOME/dev/secureframe-1"
+  for i in 2 3 4 5 6 7 8 9 10; do
+    tmux new-window -t "$session:$i" -n "sf-$i" -c "$HOME/dev/secureframe-$i"
+  done
+  tmux select-window -t "$session:1"
+
+  if [ -n "$TMUX" ]; then
+    tmux switch-client -t "$session"
+  else
+    tmux attach -t "$session"
+  fi
+}
